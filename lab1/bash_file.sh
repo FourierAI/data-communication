af6b6a000d4afd8e0463c3164ec35e0fef9015bd
MrYezhipeng <939647181@qq.com>
2019-11-15 00:15:33 +0800

    change packer numbers

--- lab1/bash_file.sh
+++ lab1/bash_file.sh
@@ -2,17 +2,17 @@
 
 for i in $(seq 5 5 95);
 do echo $i ;
-python new_mm1.py -M 1 -A $i -S 100 --no-trace >> mm1.out
+python new_mmN.py -M 1 -A $i -S 100 -N 10000 --no-trace >> mm1.out
 done
 
 for i in $(seq 5 5 95);
 do echo $i ;
-python new_mm1.py -M 2 -A $i -S 50 --no-trace >> mm2.out
+python new_mmN.py -M 2 -A $i -S 50 -N 10000 --no-trace >> mm2.out
 done
 
 for i in $(seq 5 5 95);
 do echo $i ;
-python new_mm1.py -M 5 -A $i -S 20 --no-trace >> mm5.out
+python new_mmN.py -M 5 -A $i -S 20 -N 10000 --no-trace >> mm5.out
 done
 
 python plot_mmN.py -F 'mm1.out'
