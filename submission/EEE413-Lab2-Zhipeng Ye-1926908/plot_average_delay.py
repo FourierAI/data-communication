4c50c59fd0e9986c25083b60de6160a1d1318161
MrYezhipeng <939647181@qq.com>
2019-12-21 11:54:36 +0800

    complete assignment 2

---
+++ submission/EEE413-Lab2-Zhipeng Ye-1926908/plot_average_delay.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == "__main__":
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    # Or if you want different settings for the grids:
+    ax.grid(which='minor', alpha=0.2)
+    ax.grid(which='major', alpha=0.5)
+
+    # calculate and plot analytical results
+
+    # load and plot simulation results
+    # change delimiter '/t' into ','
+    x, y = np.loadtxt('wait_time.out', delimiter=' = ', unpack=True)
+
+    plt.plot(x, y)
+    plt.xlabel('packet size B')
+    plt.ylabel('waiting time ms')
+
+    # add labels, legend, and title
+    plt.legend()
+    plt.title(r'variation of waiting time with the packet size')
+
+    plt.savefig('waiting time.pdf')
+    plt.show()
